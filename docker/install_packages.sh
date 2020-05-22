964d76eadfe9348d33ac24806febfa95e6794690
Fagner Pimentel <fagnerpimentel@gmail.com>
2020-05-22 20:06:18 -0300

    update experiments sets

--- docker/install_packages.sh
+++ docker/install_packages.sh
@@ -8,12 +8,12 @@
 mkdir $CATKIN_PATH/src
 cd $CATKIN_PATH/src
 
-git clone https://fagnerpimentel@gitlab.com/fpimentel/hera/hera_description.git
-git clone https://fagnerpimentel@gitlab.com/fpimentel/hera/hera_nav.git
-git clone https://fagnerpimentel@gitlab.com/fpimentel/hera/hera_bringup.git
+git clone https://gitlab.com/fpimentel/hera/hera_description.git
+git clone https://gitlab.com/fpimentel/hera/hera_nav.git
+git clone https://gitlab.com/fpimentel/hera/hera_bringup.git
 
-git clone https://fagnerpimentel@gitlab.com/fpimentel/social_robot/social_worlds.git
-git clone https://fagnerpimentel@gitlab.com/fpimentel/social_robot/social_experiments.git
+git clone https://gitlab.com/fpimentel/social_robot/social_worlds.git
+git clone https://gitlab.com/fpimentel/social_robot/social_experiments.git
 
 apt-get update
 ./hera_description/install_dependencies.sh
