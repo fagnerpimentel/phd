9b35a79576a5f5476cdb7a16307adc553da82017
Fagner Pimentel <fagnerpimentel@gmail.com>
2020-06-01 10:11:06 -0300

    add navigation_layers to install_packages

--- docker/install_packages.sh
+++ docker/install_packages.sh
@@ -15,6 +15,8 @@ git clone https://gitlab.com/fpimentel/hera/hera_bringup.git
 git clone https://gitlab.com/fpimentel/social_robot/social_worlds.git
 git clone https://gitlab.com/fpimentel/social_robot/social_experiments.git
 
+git clone https://github.com/DLu/navigation_layers.git
+
 apt-get update
 ./hera_description/install_dependencies.sh
 ./hera_nav/install_dependencies.sh
